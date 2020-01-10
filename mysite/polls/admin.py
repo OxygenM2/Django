9d491babf465bc4eeb01ae6b52983d8004f187b4
jackson-abc <wefhung@163.com>
2020-01-10 16:27:45 +0800

    authorize

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Questions
+from .models import Question
 
-admin.site.register(Questions)
+admin.site.register(Question)
